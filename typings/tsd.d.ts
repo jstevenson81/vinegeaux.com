2f71e157f9150b0a3c2a527ad57fe2b39a895e61
Jonathan Stevenson <jstevenson@retinalscreenings.com>
2015-10-03 21:43:51 -0500

    more code work

---
+++ typings/tsd.d.ts
@@ -0,0 +1,7 @@
+
+/// <reference path="angularjs/angular-animate.d.ts" />
+/// <reference path="angularjs/angular-route.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="bootstrap/bootstrap.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="underscore/underscore.d.ts" />
